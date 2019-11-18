5f68843cce52801bdcb532f828e65cd8919f0e25
jocvegar <jocvegar@gmail.com>
2019-11-18 15:06:02 -0600

    Adds serviceWorker

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  root 'home#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/service-worker.js' => "service_worker#service_worker"
+  get '/manifest.json' => "service_worker#manifest"
+  get '/offline.html' => "service_worker#offline"
 end
+
