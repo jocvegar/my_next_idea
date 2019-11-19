7e77d3d4dcce2c989ac06b255b0d5be7fce09998
jocvegar <jocvegar@gmail.com>
2019-11-19 09:01:56 -0600

    loads stylesheet from webpacker

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,6 +11,7 @@ require("custom/index")
 
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '../stylesheets/application'
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
